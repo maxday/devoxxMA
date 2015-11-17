087e87c12bfa9e42ff49be299436450f9a108105
maxday <maxday@MacBook-Pro-de-maxday.local>
2015-11-17 02:48:16 +0000

    grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,17 +4,32 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-connect');
 
+  //var myCustomHeader = grunt.option('myCustomHeader') || 'gruntPower';
+  //var shouldLog = grunt.option('shouldLog') || false;
+
   grunt.initConfig({
     connect: {
       server: {
         options: {
           keepalive: true,
           port: 9000,
-          hostname: 'localhost'
+          hostname: 'localhost',
+          middleware: function(connect, options, middlewares) {
+            middlewares.unshift(function(req, res, next) {
+              /*if (shouldLog) {
+                grunt.log.writeln("request = " + req.url);
+              }
+              res.setHeader('X-MyCustomHeader', myCustomHeader);
+              */
+              res.setHeader('X-MyCustomHeader', 'helloDevoxx');
+              next();
+            });
+            return middlewares;
+          }
         }
       }
     }
-    });
+  });
 
   grunt.registerTask('run', [
     'connect:server',
