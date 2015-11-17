298da7c0e1422ccc13001d96b0dcdf756933035a
maxday <maxday@MacBook-Pro-de-maxday.local>
2015-11-17 03:38:30 +0000

    grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,6 +8,14 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-karma');
   grunt.loadNpmTasks('grunt-nightwatch');
 
+  grunt.loadNpmTasks('grunt-usemin');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+
   //var myCustomHeader = grunt.option('myCustomHeader') || 'gruntPower';
   //var shouldLog = grunt.option('shouldLog') || false;
 
@@ -94,7 +102,54 @@ module.exports = function(grunt) {
           }
         }
       }
-    }
+    },
+
+    useminPrepare: {
+      html: 'index.html',
+      options: {
+        dest: 'dist',
+        flow: {
+          html: {
+            steps: {
+              js: ['concat', 'uglify'],
+              css: ['cssmin']
+            }
+          }
+        }
+      }
+    },
+
+    usemin: {
+      html: ['dist/index.html'],
+      css: ['styles/*.css'],
+      js: ['js/*.js']
+    },
+
+    imagemin: {
+      static: {
+        options: {
+          optimizationLevel: 1,
+        },
+        files: {
+          'generatedImages/pic1.jpg': 'images/pic1.jpg',
+          'generatedImages/pic2.jpg': 'images/pic2.jpg',
+          'generatedImages/pic3.jpg': 'images/pic3.jpg'
+        }
+      }
+    },
+    
+    copy: {
+      html: {
+        src: 'index.html',
+        dest: 'dist/index.html'
+      },
+      images: {
+        cwd: 'generatedImages/',
+        src: '*.jpg',
+        dest: 'dist/images/',
+        expand: true
+      }
+    },
 
   });
 
@@ -117,5 +172,17 @@ module.exports = function(grunt) {
     'connect:test',
     'nightwatch'
   ]);
+
+  grunt.registerTask('build', [
+    'compass',
+    'copy:html',
+    'copy:images',
+    'useminPrepare',
+    'concat',
+    'uglify',
+    'cssmin',
+    'imagemin',
+    'usemin'
+  ])
 
 };
